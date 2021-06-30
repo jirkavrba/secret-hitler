96795f16daa3cf8a7e2f648c99d7bcc0869316df
Jiří Vrba <jiri@vrba.dev>
2021-06-30 08:38:09 +0200

    Initial commit

---
+++ src/main/java/dev/vrba/secrethitler/SecretHitlerApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.secrethitler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretHitlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretHitlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vrba/secrethitler/SecretHitlerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.secrethitler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretHitlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
