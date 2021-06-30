7372a472d0f6616c915a833854b8a408108bcc2c
Jiří Vrba <jiri@vrba.dev>
2021-06-30 14:50:29 +0200

    Player state implementation + public representation

---
+++ src/main/java/dev/vrba/secrethitler/engine/state/PublicPlayerState.java
@@ -0,0 +1,19 @@
+package dev.vrba.secrethitler.engine.state;
+
+import dev.vrba.secrethitler.engine.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class PublicPlayerState {
+
+    private final @NotNull UUID id;
+
+    private final boolean alive;
+
+    private final @NotNull Role role;
+}
