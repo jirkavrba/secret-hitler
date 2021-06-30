df8e7c2a829ada3ac853ae0fce5ba988081f2267
Jiří Vrba <jiri@vrba.dev>
2021-06-30 09:08:43 +0200

    Add enum for presidential abilities unlocked by enacting fascist policies

---
+++ src/main/java/dev/vrba/secrethitler/engine/election/PresidentialAbility.java
@@ -0,0 +1,8 @@
+package dev.vrba.secrethitler.engine.election;
+
+public enum PresidentialAbility {
+    PEEK_TOP_THREE_POLICIES,
+    INVESTIGATE_LOYALTY,
+    CALL_SPECIAL_ELECTION,
+    EXECUTE_PLAYER;
+}
