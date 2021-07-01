622b659186492d33928a4c8214f633a203a0cd01
Jiří Vrba <jiri@vrba.dev>
2021-07-01 21:46:49 +0200

    Implement presidential abilities

--- client/src/components/game/EnactedPolicies.vue
+++ client/src/components/game/EnactedPolicies.vue
@@ -6,8 +6,10 @@
           <img src="../../assets/policies/liberal.png" alt="Fascist policy"
                :class="`transform filter -translate-y-2 scale-75 drop-shadow-lg ${rotation(i)}`">
         </div>
-        <div v-for="i in (5 - liberal)" :key="i"
-             class=" m-3 rounded w-24 h-32 border-2 border-blue-500 border-dashed"></div>
+        <div v-for="i in (4 - liberal)" :key="i" class=" m-3 rounded w-24 h-32 border-2 border-blue-500 border-dashed"></div>
+        <div class="m-3 rounded w-24 h-32 border-2 border-blue-500 bg-blue-800 flex items-center justify-center">
+          <img src="../../assets/liberals.png" class="w-20">
+        </div>
       </div>
 
       <div class="bg-red-900 filter saturate-50 p-5 rounded flex mt-5">
@@ -15,8 +17,14 @@
           <img src="../../assets/policies/fascist.png" alt="Fascist policy"
                :class="`transform filter -translate-y-2 scale-75 drop-shadow-lg ${rotation(5 + i)}`">
         </div>
-        <div v-for="i in (6 - fascist)" :key="i"
-             class="m-3 rounded w-24 h-32 border-2 border-red-500 border-dashed"></div>
+        <div v-for="i in (5 - fascist)" :key="i" class="m-3 rounded w-24 h-32 border-2 border-red-500 border-dashed flex items-center justify-center">
+          <div v-if="ability(fascist + i) !== null" class="w-12">
+            <img :src="ability(fascist + i).image" :alt="ability(fascist + i).text" :title="ability(fascist + i).text">
+          </div>
+        </div>
+        <div class="m-3 rounded w-24 h-32 border-2 border-red-500 bg-red-800 flex items-center justify-center">
+          <img src="../../assets/fascists.png" class="w-20">
+        </div>
       </div>
     </div>
   </div>
@@ -24,13 +32,35 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import peekPolicies from "../../assets/abilities/peek-policies.png";
+import executePlayer from "../../assets/abilities/execute-player.png";
+import investigateLoyalty from "../../assets/abilities/investigate-loyalty.png";
+import callSpecialElection from "../../assets/abilities/call-special-election.png";
 
 const multiplies = [-6, -3, -2, 2, 3, 6];
 
 export default defineComponent({
   name: "EnactedPolicies",
   data: () => ({
-    rotations: (new Array(11).fill(0).map(() => multiplies[Math.floor(Math.random() * multiplies.length)]))
+    rotations: (new Array(11).fill(0).map(() => multiplies[Math.floor(Math.random() * multiplies.length)])),
+    abilities: {
+      "PEEK_TOP_THREE_POLICIES": {
+        image: peekPolicies,
+        text: "The president examines top three cards."
+      },
+      "EXECUTE_PLAYER": {
+        image: executePlayer,
+        text: "The president must kill a player."
+      },
+      "CALL_SPECIAL_ELECTION": {
+        image: callSpecialElection,
+        text: "The president must select the next presidential candidate."
+      },
+      "INVESTIGATE_LOYALTY": {
+        image: investigateLoyalty,
+        text: "The president must investigate party membership of one of the players."
+      }
+    }
   }),
   methods: {
     rotation(i) {
@@ -41,7 +71,18 @@ export default defineComponent({
       }
 
       return `rotate-${rotation}`;
-    }
+    },
+    ability(i) {
+      const target = this.$store.state.game.gameState.enactedPolicies.abilities[i];
+
+      if (typeof target === "undefined") {
+        return null;
+      }
+
+      console.log(this.abilities[target].text);
+
+      return this.abilities[target];
+    },
   },
   computed: {
     liberal() {
--- client/src/store.ts
+++ client/src/store.ts
@@ -42,6 +42,11 @@ export default createStore({
                         username: "Another Jeff",
                         alive: true,
                     },
+                    "34c3cac2-8009-4470-aa0b-1d0ab5ee0270": {
+                        id: "34c3cac2-8009-4470-aa0b-1d0ab5ee0270",
+                        username: "Jezz",
+                        alive: true,
+                    },
                     "5e06a048-866c-4edf-9bdd-8e6ee3b98c10": {
                         id: "5e06a048-866c-4edf-9bdd-8e6ee3b98c10",
                         username: "Not Jeff at all",
@@ -54,8 +59,14 @@ export default createStore({
                     }
                 },
                 enactedPolicies: {
-                    fascist: 2,
-                    liberal: 3
+                    fascist: 1,
+                    liberal: 3,
+                    abilities: {
+                        2: "INVESTIGATE_LOYALTY",
+                        3: "PEEK_TOP_THREE_POLICIES",
+                        4: "CALL_SPECIAL_ELECTION",
+                        5: "EXECUTE_PLAYER"
+                    }
                 },
                 electionTracker: 0,
                 drawingDeckCards: 3,
@@ -73,6 +84,7 @@ export default createStore({
                         "3cba6f3c-487f-40d0-a9fb-cdc43c88adfd": "NONE",
                         "8ddd8049-155b-4980-b7e6-a79de1e0a20c": "NONE",
                         "33c3cac2-8009-4470-aa0b-1d0ab5ee0270": "HIDDEN",
+                        "34c3cac2-8009-4470-aa0b-1d0ab5ee0270": "NONE",
                         "5e06a048-866c-4edf-9bdd-8e6ee3b98c10": "HIDDEN",
                         "27f1f378-3b4e-428d-8639-766bac14f619": "NONE"
                     }
@@ -86,6 +98,7 @@ export default createStore({
                     "3cba6f3c-487f-40d0-a9fb-cdc43c88adfd": "HIDDEN",
                     "8ddd8049-155b-4980-b7e6-a79de1e0a20c": "LIBERAL",
                     "33c3cac2-8009-4470-aa0b-1d0ab5ee0270": "HITLER",
+                    "34c3cac2-8009-4470-aa0b-1d0ab5ee0270": "HIDDEN",
                     "5e06a048-866c-4edf-9bdd-8e6ee3b98c10": "FASCIST",
                     "27f1f378-3b4e-428d-8639-766bac14f619": "LIBERAL"
                 },
