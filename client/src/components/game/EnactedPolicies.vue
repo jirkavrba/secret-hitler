2a98fb2ad7f3d3a3207894f955bcc4358894e350
Jiří Vrba <jiri@vrba.dev>
2021-07-01 18:19:15 +0200

    Work in progress implementaion of enacted policies

---
+++ client/src/components/game/EnactedPolicies.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="p-3">
+    <div class="rounded p-5">
+      <div class="bg-blue-100 p-5 rounded flex">
+        <div v-for="i in liberal" :key="i" class="bg-blue-500 m-3 rounded w-24 h-32"></div>
+        <div v-for="i in (5 - liberal)" :key="i" class="bg-blue-50 m-3 rounded w-24 h-32"></div>
+      </div>
+
+      <div class="bg-red-100 p-5 rounded flex mt-5">
+        <div v-for="i in fascist" :key="i" class="bg-red-500 m-3 rounded w-24 h-32"></div>
+        <div v-for="i in (6 - fascist)" :key="i"
+             class="bg-red-50 m-3 rounded w-24 h-32"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "EnactedPolicies",
+  computed: {
+    liberal() {
+      return this.$store.state.game.gameState.enactedPolicies.liberal;
+    },
+    fascist() {
+      return this.$store.state.game.gameState.enactedPolicies.fascist;
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="grid grid-cols-5 container">
     <Players/>
-    <div class="col-span-3 flex flex-col bg-green-300"></div>
+    <div class="col-span-3 flex flex-col">
+      <EnactedPolicies/>
+    </div>
     <div class="bg-green-500">CHAT</div>
   </div>
 </template>
@@ -16,10 +18,12 @@
 <script lang="ts">
 import {defineComponent} from 'vue';
 import Players from "../components/game/Players.vue";
+import EnactedPolicies from "../components/game/EnactedPolicies.vue";
 
 export default defineComponent({
   name: "Game",
   components: {
+    EnactedPolicies,
     Players
   }
 });
