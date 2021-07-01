1cd91e9a38656bebfc84b69190e167e2eceec8a0
Jiří Vrba <jiri@vrba.dev>
2021-07-01 16:15:32 +0200

    Better role colors

--- client/src/components/game/Player.vue
+++ client/src/components/game/Player.vue
@@ -1,7 +1,9 @@
 <template>
-  <div :class="'mt-3 px-5 py-3 rounded-lg flex flex-row items-center ' + colors[role]">
+  <div :class="'mt-3 px-5 py-3 rounded-lg flex flex-row items-center ' + colors[role].background">
     <img :src="roles[role]" :alt="role" class="w-12 mr-3">
-    <div class="flex-grow bg-gray-300">{{ role }}</div>
+    <div class="flex-grow">
+      <span :class="'uppercase font-black text-sm tracking-wide ' + colors[role].foreground">{{ role }}</span>
+    </div>
   </div>
 </template>
 
@@ -22,10 +24,22 @@ export default defineComponent({
   ],
   data: () => ({
     colors: {
-      "liberal": "bg-blue-100",
-      "fascist": "bg-red-100",
-      "hitler": "bg-red-200",
-      "hidden": "bg-gray-200"
+      "liberal": {
+        background: "bg-blue-100",
+        foreground: "text-blue-700",
+      },
+      "fascist": {
+        background: "bg-red-100",
+        foreground: "text-red-700"
+      },
+      "hitler": {
+        background: "bg-gray-800",
+        foreground: "text-red-500",
+      },
+      "hidden": {
+        background: "bg-gray-200",
+        foreground: "text-gray-700"
+      }
     },
     roles: {
       liberal,
