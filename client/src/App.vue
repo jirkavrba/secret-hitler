bb307334c2190ff93a651bf08b968c848f9dc3c4
Jiří Vrba <jiri@vrba.dev>
2021-07-01 14:03:11 +0200

    Mobile incompatibility note

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <router-view/>
+  <div class="app-container">
+    <div class="lg:hidden block bg-black flex flex-col items-center justify-center app-container">
+      <img alt="Secret Hitler logo" src="./assets/logo.png" class="w-64">
+
+      <div class="my-16">
+        <h1 class="text-white text-3xl font-black">Sorry,</h1>
+        <h2 class="text-gray-300">
+          This shit doesn't work on mobiles
+          <span class="text-gray-500">(yet?)</span>
+        </h2>
+      </div>
+    </div>
+    <div class="hidden lg:block">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
+<style>
+.app-container {
+  min-height: 100vh;
+}
+</style>
+
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 
 export default defineComponent({
   name: 'App',
