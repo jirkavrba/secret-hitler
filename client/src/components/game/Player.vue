8575bef0a181162ae0836443067e4b8599d53b13
Jiří Vrba <jiri@vrba.dev>
2021-07-01 14:38:14 +0200

    Basic player component

---
+++ client/src/components/game/Player.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ id }}
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Player",
+  props: [
+    "id"
+  ]
+})
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/game/Players.vue
+++ client/src/components/game/Players.vue
@@ -2,15 +2,24 @@
   <div class="p-3">
     <div class="rounded bg-gray-50 p-5">
       <h1 class="text-md font-bold uppercase tracking-wide text-gray-500">Players</h1>
+
+      <div>
+        <Player v-for="player in Object.keys($store.state.game.gameState.players)"
+                :key="player"
+                :id="player"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import Player from "./Player.vue";
 
 export default defineComponent({
-  name: "Players"
+  name: "Players",
+  components: {Player}
 })
 </script>
 
