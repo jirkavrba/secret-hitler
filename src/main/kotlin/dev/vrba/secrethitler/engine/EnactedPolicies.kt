61147576936f691aec5c8d9282b4874f0f5d9e1c
Jiří Vrba <jiri@vrba.dev>
2021-07-06 07:45:05 +0200

    Implement presidential abilities

--- src/main/kotlin/dev/vrba/secrethitler/engine/EnactedPolicies.kt
+++ src/main/kotlin/dev/vrba/secrethitler/engine/EnactedPolicies.kt
@@ -1,16 +1,12 @@
 package dev.vrba.secrethitler.engine
 
-import dev.vrba.secrethitler.original.election.PresidentialAbility
-import org.jetbrains.annotations.Contract
-
 const val requiredFascistPolicies = 6
 const val requiredLiberalPolicies = 5
 
 data class EnactedPolicies(val fascist: Int, val liberal: Int, val abilities: Map<Int, PresidentialAbility>) {
 
-    constructor(players: Int) : this(0, 0, PresidentialAbility.getAbilities(players))
+    constructor(players: Int) : this(0, 0, getPresidentialAbilities(players))
 
-    @Contract(pure = true)
     fun addPolicy(policy: Policy): EnactedPolicies =
         when (policy) {
             Policy.Fascist -> EnactedPolicies(fascist + 1, liberal, abilities)
