8440c6d6f66ae3a05f0af4827924c2bac651e06e
Jiří Vrba <jiri@vrba.dev>
2021-07-09 17:37:42 +0200

    Add Party enum

---
+++ SecretHitler/Engine/Party.cs
@@ -0,0 +1,13 @@
+namespace SecretHitler.Engine
+{
+    /// <summary>
+    /// Enum representing the political party in the game
+    /// Also this enum is used to represent policies, membership and everything else related here
+    /// </summary>
+    public enum Party
+    {
+        Fascist,
+        Liberal,
+        Hidden
+    }
+}
