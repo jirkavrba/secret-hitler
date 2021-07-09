bdc22bc6a58218d45e8c0c38e1271dac7c0e3c1e
Jiří Vrba <jiri@vrba.dev>
2021-07-09 17:35:12 +0200

    Apply resharper-provided solutions

--- SecretHitler/Startup.cs
+++ SecretHitler/Startup.cs
@@ -21,10 +21,10 @@ namespace SecretHitler
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -34,7 +34,7 @@ namespace SecretHitler
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
