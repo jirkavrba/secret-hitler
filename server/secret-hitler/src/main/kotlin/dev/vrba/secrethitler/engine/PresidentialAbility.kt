3c10b1fdecd89a44d039e1b6a1fd32f6d736ea60
Jiří Vrba <jiri@vrba.dev>
2021-07-10 09:36:12 +0200

    Add method for generating presidential abilities map based on number of players

--- server/secret-hitler/src/main/kotlin/dev/vrba/secrethitler/engine/PresidentialAbility.kt
+++ server/secret-hitler/src/main/kotlin/dev/vrba/secrethitler/engine/PresidentialAbility.kt
@@ -1,5 +1,28 @@
 package dev.vrba.secrethitler.engine
 
+fun getPresidentialAbilities(players: Int): Map<Int, PresidentialAbility> =
+    when (players) {
+        5, 6 -> mapOf(
+            3 to PresidentialAbility.PeekPolicies,
+            4 to PresidentialAbility.ExecutePlayer,
+            5 to PresidentialAbility.ExecutePlayer,
+        )
+        7, 8 -> mapOf(
+            2 to PresidentialAbility.InvestigatePlayerLoyalty,
+            3 to PresidentialAbility.CallSpecialElection,
+            4 to PresidentialAbility.ExecutePlayer,
+            5 to PresidentialAbility.ExecutePlayer
+        )
+        9, 10 -> mapOf(
+            1 to PresidentialAbility.InvestigatePlayerLoyalty,
+            2 to PresidentialAbility.InvestigatePlayerLoyalty,
+            3 to PresidentialAbility.CallSpecialElection,
+            4 to PresidentialAbility.ExecutePlayer,
+            5 to PresidentialAbility.ExecutePlayer
+        )
+        else -> throw IllegalArgumentException("Number of players must be between 5 and 10 inclusive.")
+    }
+
 enum class PresidentialAbility {
     PeekPolicies,
     InvestigatePlayerLoyalty,
