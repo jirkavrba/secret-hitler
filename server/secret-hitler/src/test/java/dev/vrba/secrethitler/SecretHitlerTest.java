f6e9b0be6cf62242e9e126de56c3b86ef62c43cd
Jiří Vrba <jiri@vrba.dev>
2021-07-10 08:23:33 +0200

    Fresh micronaut application scaffold

---
+++ server/secret-hitler/src/test/java/dev/vrba/secrethitler/SecretHitlerTest.java
@@ -0,0 +1,21 @@
+package dev.vrba.secrethitler;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class SecretHitlerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
