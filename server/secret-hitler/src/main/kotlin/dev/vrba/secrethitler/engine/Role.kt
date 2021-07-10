a976e17be5d7825c7f5b4bfdf58129e84b550410
Jiří Vrba <jiri@vrba.dev>
2021-07-10 08:52:50 +0200

    Add convenience method for creating a role pool based on number of players

--- server/secret-hitler/src/main/kotlin/dev/vrba/secrethitler/engine/Role.kt
+++ server/secret-hitler/src/main/kotlin/dev/vrba/secrethitler/engine/Role.kt
@@ -1,5 +1,24 @@
 package dev.vrba.secrethitler.engine
 
+fun createRolePool(players: Int): List<Role> =
+    when (players) {
+        5 -> createRolePool(1, 3)
+        6 -> createRolePool(1, 4)
+        7 -> createRolePool(2, 4)
+        8 -> createRolePool(2, 5)
+        9 -> createRolePool(3, 5)
+        10 -> createRolePool(3, 6)
+        else -> throw IllegalArgumentException("Number of players must be between 5 and 10 inclusive.")
+    }
+
+private fun createRolePool(fascists: Int, liberals: Int): List<Role> {
+    val pool = listOf(Role.Hitler) +
+            List(fascists) { Role.Fascist } +
+            List(liberals) { Role.Liberal }
+
+    return pool.shuffled()
+}
+
 enum class Role(val partyMembership: Party) {
     Hitler(Party.Fascist),
     Fascist(Party.Fascist),
